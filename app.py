13eb3d8563cda4014ac88fce29b6c1a6beeef7d1
kazurei <167180368+kazurei@users.noreply.github.com>
2026-05-14 08:17:55 +0000

    yoi

--- app.py
+++ app.py
@@ -1,6 +1,64 @@
+##import streamlit as st
+##import pandas as pd
+##
+##from app_store_reviews_reader import AppStoreReviewsReader
+##
+##st.title("App Store レビュー取得")
+##
+##app_id = st.text_input(
+##    "App ID",
+##    "1325457827"
+##)
+##
+##country = st.selectbox(
+##    "国",
+##    ["jp", "us"]
+##)
+##
+##review_count = st.slider(
+##    "取得件数",
+##    10,
+##    1000,
+##    100
+##)
+##
+##if st.button("取得開始"):
+##
+##    try:
+##
+##        reader = AppStoreReviewsReader()
+##
+##        reviews = reader.reviews(
+##            app_id=app_id,
+##            country=country
+##        )
+##
+##        df = pd.DataFrame(reviews)
+##
+##        df = df.head(review_count)
+##
+##        st.success(
+##            f"{len(df)}件取得"
+##        )
+##
+##        st.dataframe(df)
+##
+##        csv = df.to_csv(
+##            index=False,
+##            encoding="utf-8-sig"
+##        ).encode("utf-8-sig")
+##
+##        st.download_button(
+##            "CSVダウンロード",
+##            csv,
+##            "reviews.csv",
+##            "text/csv"
+##        )
+##
+##    except Exception as e:
+##        st.error(e)
 import streamlit as st
 import pandas as pd
-
 from app_store_reviews_reader import AppStoreReviewsReader
 
 st.title("App Store レビュー取得")
@@ -10,50 +68,15 @@ app_id = st.text_input(
     "1325457827"
 )
 
-country = st.selectbox(
-    "国",
-    ["jp", "us"]
-)
+if st.button("取得"):
 
-review_count = st.slider(
-    "取得件数",
-    10,
-    1000,
-    100
-)
+    reader = AppStoreReviewsReader()
 
-if st.button("取得開始"):
+    reviews = reader.reviews(
+        app_id=app_id,
+        country="jp"
+    )
 
-    try:
+    df = pd.DataFrame(reviews)
 
-        reader = AppStoreReviewsReader()
-
-        reviews = reader.reviews(
-            app_id=app_id,
-            country=country
-        )
-
-        df = pd.DataFrame(reviews)
-
-        df = df.head(review_count)
-
-        st.success(
-            f"{len(df)}件取得"
-        )
-
-        st.dataframe(df)
-
-        csv = df.to_csv(
-            index=False,
-            encoding="utf-8-sig"
-        ).encode("utf-8-sig")
-
-        st.download_button(
-            "CSVダウンロード",
-            csv,
-            "reviews.csv",
-            "text/csv"
-        )
-
-    except Exception as e:
-        st.error(e)
+    st.dataframe(df)
