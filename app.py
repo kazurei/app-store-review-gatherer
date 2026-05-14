9f4b8c566ca18bad78153d5c0e9a7c38178b3fd7
kazurei <167180368+kazurei@users.noreply.github.com>
2026-05-14 08:16:49 +0000

    ｙわあ

--- app.py
+++ app.py
@@ -1,59 +1,59 @@
-##import streamlit as st
-##import pandas as pd
-##
-##from app_store_reviews_reader import AppStoreReviewsReader
-##
-##st.title("App Store レビュー取得")
-##
-##app_id = st.text_input(
-##    "App ID",
-##    "1325457827"
-##)
-##
-##country = st.selectbox(
-##    "国",
-##    ["jp", "us"]
-##)
-##
-##review_count = st.slider(
-##    "取得件数",
-##    10,
-##    1000,
-##    100
-##)
-##
-##if st.button("取得開始"):
-##
-##    try:
-##
-##        reader = AppStoreReviewsReader()
-##
-##        reviews = reader.reviews(
-##            app_id=app_id,
-##            country=country
-##        )
-##
-##        df = pd.DataFrame(reviews)
-##
-##        df = df.head(review_count)
-##
-##        st.success(
-##            f"{len(df)}件取得"
-##        )
-##
-##        st.dataframe(df)
-##
-##        csv = df.to_csv(
-##            index=False,
-##            encoding="utf-8-sig"
-##        ).encode("utf-8-sig")
-##
-##        st.download_button(
-##            "CSVダウンロード",
-##            csv,
-##            "reviews.csv",
-##            "text/csv"
-##        )
-##
-##    except Exception as e:
-##        st.error(e)
+import streamlit as st
+import pandas as pd
+
+from app_store_reviews_reader import AppStoreReviewsReader
+
+st.title("App Store レビュー取得")
+
+app_id = st.text_input(
+    "App ID",
+    "1325457827"
+)
+
+country = st.selectbox(
+    "国",
+    ["jp", "us"]
+)
+
+review_count = st.slider(
+    "取得件数",
+    10,
+    1000,
+    100
+)
+
+if st.button("取得開始"):
+
+    try:
+
+        reader = AppStoreReviewsReader()
+
+        reviews = reader.reviews(
+            app_id=app_id,
+            country=country
+        )
+
+        df = pd.DataFrame(reviews)
+
+        df = df.head(review_count)
+
+        st.success(
+            f"{len(df)}件取得"
+        )
+
+        st.dataframe(df)
+
+        csv = df.to_csv(
+            index=False,
+            encoding="utf-8-sig"
+        ).encode("utf-8-sig")
+
+        st.download_button(
+            "CSVダウンロード",
+            csv,
+            "reviews.csv",
+            "text/csv"
+        )
+
+    except Exception as e:
+        st.error(e)
